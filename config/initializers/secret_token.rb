b2ad58a8ef991aa327cca0ef0a4f5f4775d459bc
peter zhang nz <vmt024@gmail.com>
2012-04-27 23:14:49 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RaidBilling::Application.config.secret_token = '3660d236d1d2b0a2a44b1e802d74b24f77708d6ca5e179e7af244e1194aa2af3f3a22a5260ee3b0972bf0b3f14402009a8df87b881aacfee087fa5ea5d4c2879'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RaidBilling::Application.config.session_store :cookie_store, :key => '_raid_billing_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RaidBilling::Application.config.session_store :active_record_store
